75c7e84fedca5be1daf20ddcd9a601aef2c42e04
mineachem <minea.chem@gmail.com>
2016-05-25 15:43:04 +0700

    hello commit

--- TestNested/src/Outer.java
+++ TestNested/src/Outer.java
@@ -20,7 +20,10 @@ public class Outer
       } 
     } 
     
+    
     public static void main(String[] args) {
+    	
+    	System.out.println("hello world");
 		Outer obj=new Outer();
 		obj.show();
 	}
